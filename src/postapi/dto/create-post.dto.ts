78f86efc2171335f4b617e1427affd1063958cd2
Diana <37293613+programmistik@users.noreply.github.com>
2021-06-21 21:09:27 +0400

    Update create-post.dto.ts

--- src/postapi/dto/create-post.dto.ts
+++ src/postapi/dto/create-post.dto.ts
@@ -1,5 +1,6 @@
 export class CreatePostDTO {
     readonly Id: string;
+    readonly UniqueId: string;
     readonly ProfileId: string;
     readonly Image: string;
     readonly Title: string;
@@ -7,5 +8,6 @@ export class CreatePostDTO {
     readonly Date: string;
     readonly Description: string;
     readonly ViewsProfileId: Array<string>;
-    readonly LikesProfileId: Array<string>
+    readonly LikesProfileId: Array<string>;
+    readonly Hashtags: Array<string>
 }
