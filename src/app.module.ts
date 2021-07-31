20962e36543cc75e1496a6d0c0c0cc78b65e44b2
Enkeli __ <programmistik@hotmail.com>
2021-07-31 13:04:07 +0400

    --

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostapiModule } from './postapi/postapi.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost/postapi', { useNewUrlParser: true, useFindAndModify: false }), PostapiModule,],
+    imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DBURL, { useNewUrlParser: true, useFindAndModify: false }), PostapiModule,],
   controllers: [AppController],
   providers: [AppService],
 })
